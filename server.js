337e064400896c2f94aa7a0e74ae32c2fbb0671e
bryanhill8 <bryanhill_7@yahoo.com>
2015-09-17 03:25:24 +0000

    second commit

--- server.js
+++ server.js
@@ -9,14 +9,35 @@ var fortune = function(req, res, next) {
 
     var cookie = fs.readFile("fortunecookie.txt");
     
-   // var random = Math.floor((Math.random() * cookie.length));
-  //res.write(cookie[random]);
+    var random = Math.floor((Math.random() * cookie.length));
+  res.write(cookie[random]);
     
-    res.end();
+    next();
 };
 
 
 app.use(fortune);
+
+var date = new Date();
+var current_month = date.getMonth();
+
+var leo = function(req, res, next) {
+  
+  leo = current_month
+  res.write("Live, love, laugh");
+  next();
+};
+
+app.use(leo);
+
+var aries = function(req, res, next) {
+  
+  aries = current_month
+  res.write("Walk the path less traveled.");
+  next();
+};
+
+app.use(aries);
 
 
 app.listen(process.env.PORT);
